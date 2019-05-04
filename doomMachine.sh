7ae52553987b8d271c045e8ba14051126dbab565
Thang Nguyen <tnguye20@uvm.edu>
2019-05-04 12:07:24 -0400

    Added user as sudoers to makepkg

--- doomMachine.sh
+++ doomMachine.sh
@@ -16,48 +16,51 @@ else
   dotBranch=$2
 fi
 
+# Force sudo without prompting password
+sudo su - root -c  "echo \"$user     ALL=(ALL) NOPASSWD:ALL \" >> /etc/sudoers "
+
 cd /home/$user
 
 # Update packages
 pacman -Syu --noconfirm
-pacman -S bat
-pacman -S terminology
+sudo pacman -S bat --noconfirm
+sudo pacman -S terminology --noconfirm
 
 # Install Git and basic config vars
 if pacman -Qi git > /dev/null; then
   echo "Git is already installed"
 else
-  pacman -S git --noconfirm
-  su - $user -c "git config --global 'user.name' 'Thang Nguyen'"
-  su - $user -c "git config --global 'user.email' 'tnguye20@uvm.edu'"
+  sudo pacman -S git --noconfirm
+  git config --global 'user.name' 'Thang Nguyen'
+  git config --global 'user.email' 'tnguye20@uvm.edu'
 fi
 
 # Install meld and diff/merge tool
 if pacman -Qi meld > /dev/null; then
   echo "Meld is already installed"
 else
-  pacman -S meld --noconfirm
-  su - $user -c "git config --global diff.tool meld"
-  su - $user -c "git config --global difftool.meld.path '/usr/bin/meld'"
-  su - $user -c "git config --global difftool.prompt false"
-  su - $user -c "git config --global difftool.meld.cmd 'meld \"$LOCAL\" \"$REMOTE\"'"
+  sudo pacman -S meld --noconfirm
+  git config --global diff.tool meld
+  git config --global difftool.meld.path '/usr/bin/meld'
+  git config --global difftool.prompt false
+  git config --global difftool.meld.cmd 'meld \"$LOCAL\" \"$REMOTE\"'
 
-  su - $user -c "git config --global merge.tool meld"
-  su - $user -c "git config --global mergetool.meld.path '/usr/bin/meld'"
-  su - $user -c "git config --global mergetool.prompt false"
-  su - $user -c "git config --global mergetool.meld.cmd 'meld \"$LOCAL\" \"$BASE\" \"$REMOTE\" --output \"$MERGED\"'"
+  git config --global merge.tool meld
+  git config --global mergetool.meld.path '/usr/bin/meld'
+  git config --global mergetool.prompt false
+  git config --global mergetool.meld.cmd 'meld \"$LOCAL\" \"$BASE\" \"$REMOTE\" --output \"$MERGED\"'
 fi
 
 if pacman -Qi xclip > /dev/null; then
   echo "xclip is already install"
 else
-  pacman -S xclip --noconfirm
+  sudo pacman -S xclip --noconfirm
 fi
 
 if pacman -Qi feh > /dev/null; then
   echo "feh is already install"
 else
-  pacman -S feh --noconfirm
+  sudo pacman -S feh --noconfirm
 fi
 
 # ssh keygen
@@ -76,8 +79,7 @@ if pacman -Qi google-chrome > /dev/null; then
 else
   git clone https://aur.archlinux.org/google-chrome.git /home/$user/google-chrome
   cd /home/$user/google-chrome
-  pacman -U --noconfirm google-chrome-*.pkg.tar.xz
-  su - $user -c "makepkg -s"
+  makepkg -si
   cd /home/$user/
   rm -rf /home/$user/google-chrome
 fi
@@ -86,20 +88,19 @@ fi
 if pacman -Qi rxvt-unicode-pixbuf > /dev/null; then
   echo "rxvt-unicode-pixbuf is already install"
 else
-  pacman -R rxvt-unicode --noconfirm
+  sudo pacman -R rxvt-unicode --noconfirm
   git clone https://aur.archlinux.org/rxvt-unicode-pixbuf.git /home/$user/rxvt-unicode-pixbuf
   cd /home/$user/rxvt-unicode-pixbuf
-  su - $user -c "makepkg -si"
+  makepkg -si
   cd /home/$user/
   rm -rf /home/$user/rxvt-unicode-pixbuf
 fi
 
 # Tmux and oh-my-tmux
-
 if pacman -Qi tmux> /dev/null; then
   echo "tmux is already install"
 else
-  pacman -S tmux --noconfirm
+  sudo pacman -S tmux --noconfirm
 fi
 if [ -d /home/$user/.tmux ]; then
   echo "oh-my-tmux is already installed"
@@ -112,7 +113,7 @@ fi
 if [ -d /home/$user/.oh-my-zsh ]; then
   echo "oh-my-zsh is already installed"
 else
-  su - $user -c " sh -c \"\$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sed 's:env zsh -l::g' | sed 's:chsh -s .*$::g')\" "
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sed 's:env zsh -l::g' | sed 's:chsh -s .*$::g')"
 fi
 
 # antigen install
@@ -121,7 +122,7 @@ if [ -f /home/$user/antigen.zsh ]; then
 else
   curl -L git.io/antigen > /home/$user/antigen.zsh
 fi
-su - $user -c "source /home/$user/antigen.zsh"
+source /home/$user/antigen.zsh
 
 # Config Files
 if [ -d /home/$user/.dotfiles ]; then
@@ -132,7 +133,6 @@ if [ -d /home/$user/.dotfiles ]; then
 else
   git clone --single-branch -b $dotBranch https://github.com/tnguye20/.dotfiles.git /home/$user/.dotfiles
   cd /home/$user/.dotfiles/
-  git remote set-url origin git@github.com:tnguye20/.dotfiles.git
   ln -s -f /home/$user/.dotfiles/.vimrc /home/$user/
   ln -s -f /home/$user/.dotfiles/.zshrc /home/$user/
   ln -s -f /home/$user/.dotfiles/.tmux.conf.local /home/$user/
@@ -142,13 +142,16 @@ fi
 # VIM plug
 curl -fLo /home/$user/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-su - $user -c "source /home/$user/.zshrc"
-su - $user -c "vim +'PlugUpdate' +qa"
-su - $user -c "vim +'PlugInstall' +qa"
-su - $user -c "source /home/$user/.vimrc"
+source /home/$user/.zshrc
+vim +'PlugUpdate' +qa
+vim +'PlugInstall' +qa
+source /home/$user/.vimrc
 
 # Set Default Wallpaper
 feh --bg-scale /home/$user/.dotfiles/.wallpaper/mr_robot.jpg
+
+# Set ranger config
+ranger --copy-config=all
 
 # Restart i3
 i3-msg reload
